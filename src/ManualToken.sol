9d9bcbf68f5897236ab124700a5e31a43a3e4900
Dhanush R <dhanushpriya785@gmail.com>
2025-11-08 16:45:14 +0000

    chore: apply forge fmt formatting

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -2,22 +2,21 @@
 pragma solidity ^0.8.30;
 
 contract ManualToken {
-
     mapping(address => uint256) private s_balance;
 
-    function name() public pure returns(string memory) {
+    function name() public pure returns (string memory) {
         return "Manual Token";
     }
 
-    function totalSupply() public pure returns(uint256) {
+    function totalSupply() public pure returns (uint256) {
         return 100 ether;
     }
 
-    function decimals() public pure returns(uint8) {
+    function decimals() public pure returns (uint8) {
         return 18;
     }
 
-    function balanceOf(address _owner) public view returns(uint256) {
+    function balanceOf(address _owner) public view returns (uint256) {
         return s_balance[_owner];
     }
 
